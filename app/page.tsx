83395176832ebf7367c81374bb7e33fce1f19299
f4faysal <f4faysals@gmail.com>
2024-06-05 00:25:35 +0600

    Update page component to reflect "Under Construction" status

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,40 @@
+"use client";
+
 import { Hammer } from "lucide-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function UnderConstruction() {
+  const [rotated, setRotated] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRotated((prev) => !prev);
+    }, 1000); // 3000 milliseconds = 3 seconds
+
+    return () => clearInterval(interval); // Cleanup the interval
+  }, []);
+
   return (
     <div className="h-screen">
       <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
-        <Hammer size={72} />
+        <Hammer
+          size={72}
+          className={`transition-transform duration-1000 ${
+            rotated ? "rotate-45" : "rotate-0"
+          }`}
+        />
         <h1 className="text-4xl font-bold leading-tight">
           Under Construction 🚧
         </h1>
         <p className="text-center text-muted-foreground">
           Our new page is currently being built. <br />
-          Exciting updates are on the way for AdsBath!
+          Exciting updates are on the way for Adsbath!
         </p>
+        <Link href="/adsbath" className="text-blue-500 hover:underline">
+          Go back to Adsbath
+        </Link>
       </div>
     </div>
   );
 }
-
-// import { PlaneTakeoff } from "lucide-react";
-
-// export default function ComingSoon() {
-//   return (
-//     <div className="h-svh">
-//       <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
-//         <PlaneTakeoff size={72} />
-//         <h1 className="text-4xl font-bold leading-tight">Coming Soon 👀</h1>
-//         <p className="text-center text-muted-foreground">
-//           This page has not been created yet. <br />
-//           Stay tuned though!
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
