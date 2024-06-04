416043f5b5530ed36502bff2bd42f6e15edf469f
f4faysal <f4faysals@gmail.com>
2024-06-04 23:40:55 +0600

    refactor: Update page component to reflect "Under Construction" status

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,35 @@
-import { PlaneTakeoff } from "lucide-react";
+import { Hammer } from "lucide-react";
 
- 
-
-export default function ComingSoon() {
+export default function UnderConstruction() {
   return (
-    <div className="h-svh">
+    <div className="h-screen">
       <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
-        <PlaneTakeoff size={72} />
-        <h1 className="text-4xl font-bold leading-tight">Coming Soon 👀</h1>
+        <Hammer size={72} />
+        <h1 className="text-4xl font-bold leading-tight">
+          Under Construction 🚧
+        </h1>
         <p className="text-center text-muted-foreground">
-          This page has not been created yet. <br />
-          Stay tuned though!
+          Our new page is currently being built. <br />
+          Exciting updates are on the way for AdsBath!
         </p>
       </div>
     </div>
   );
 }
+
+// import { PlaneTakeoff } from "lucide-react";
+
+// export default function ComingSoon() {
+//   return (
+//     <div className="h-svh">
+//       <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
+//         <PlaneTakeoff size={72} />
+//         <h1 className="text-4xl font-bold leading-tight">Coming Soon 👀</h1>
+//         <p className="text-center text-muted-foreground">
+//           This page has not been created yet. <br />
+//           Stay tuned though!
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
