10ea823f787aa7e23ff18fccc8cdf4f7da646b10
Md Faysal Hossain <F4faysals@gmail.com>
2024-07-26 22:41:46 +0600

    add service section

---
+++ lib/data.ts
@@ -0,0 +1,44 @@
+export const services = [
+     {
+          id: 1,
+          name: "Google Ads - Get Customers and Sell",
+          description:
+               "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nostrum quasi quaerat iste cupiditate ullam earum molestiae assumenda temporafacere a expedita?",
+          url: "/google-ads.png",
+     },
+     {
+          id: 2,
+          name: "Facebook Ads - Get Customers and Sell ",
+          description:
+               "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nostrum quasi quaerat iste cupiditate ullam earum molestiae assumenda temporafacere a expedita?",
+          url: "/google-ads.png",
+     },
+     {
+          id: 3,
+          name: "Linkedin Ads - Get Customers and Sell",
+          description:
+               "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nostrum quasi quaerat iste cupiditate ullam earum molestiae assumenda temporafacere a expedita?",
+          url: "/google-ads.png",
+     },
+     {
+          id: 4,
+          name: "Google Ads - Get Customers and Sell ",
+          description:
+               "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nostrum quasi quaerat iste cupiditate ullam earum molestiae assumenda temporafacere a expedita?",
+          url: "/google-ads.png",
+     },
+     {
+          id: 5,
+          name: "Google Ads - Get Customers and Sell ",
+          description:
+               "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nostrum quasi quaerat iste cupiditate ullam earum molestiae assumenda temporafacere a expedita?",
+          url: "/google-ads.png",
+     },
+     {
+          id: 6,
+          name: "Google Ads - Get Customers and Sell ",
+          description:
+               "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nostrum quasi quaerat iste cupiditate ullam earum molestiae assumenda temporafacere a expedita?",
+          url: "/google-ads.png",
+     },
+];
