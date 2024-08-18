0e0f876fd50bd2988f212982186155a5d521703d
Md Faysal Hossain <F4faysals@gmail.com>
2024-08-18 22:07:31 +0600

    refactor: Update TheService component layout and styles

--- components/ui/the-service.tsx
+++ components/ui/the-service.tsx
@@ -2,12 +2,13 @@
 import Image from "next/image";
 import { PiArrowCircleUpRight } from "react-icons/pi";
 
-import { config } from "react-spring";
 import { services } from "@/lib/data";
-
 import CardCarousel from "../card-carousel";
 import { TbActivityHeartbeat } from "react-icons/tb";
 import Waveform from "./waveform";
+import { BiUpArrowCircle } from "react-icons/bi";
+import { IoIosStar } from "react-icons/io";
+import { FaStarHalfAlt } from "react-icons/fa";
 
 const TheService = () => {
   const Products = [
@@ -155,7 +156,7 @@ for Digital Dominance */}
       </div>
 
       {/* WhyChooseUs */}
-      <div className="container mx-auto text-white relative">
+      <div className="container mx-auto text-white relative pb-5">
         <div className=" z-10">
           <div className="container flex flex-col lg:flex-row justify-around p-6 mx-auto sm:py-16 lg:py-24">
             <div className="flex items-center justify-start w-fit relative z-10 ">
@@ -164,7 +165,7 @@ for Digital Dominance */}
                 alt="logo"
                 className="object-contain h-[400px] aspect-square rounded-2xl"
               />
-              <div className="h-40 flex flex-col  items-start justify-center aspect-square bg-[#E025CE] absolute p-5 rounded-md -right-1/2 gap-2">
+              <div className="h-40 flex flex-col  items-start justify-center aspect-square bg-[#E025CE] absolute p-5 rounded-md -right-[80px] gap-2">
                 <Waveform className="p-2 rounded-full bg-[#EE46BC] w-fit " />
                 <h5 className="text-4xl font-bold">20K+</h5>
                 <span className="text-xs">Project Done</span>
@@ -223,6 +224,72 @@ for Digital Dominance */}
 
         <div className="why-choose-us-pink absolute -left-10 top-20 -z-0" />
         <div className="why-choose-us-pale absolute right-0 top-0 -z-0" />
+      </div>
+
+      {/* Customer Review */}
+      <div className="container mx-auto text-white">
+        <div className="lg:-mt-28 md:-mt-20 -mt-10">
+          <div className="container flex flex-col lg:flex-row justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:justify-between">
+            {/* Text Section */}
+            <div className="flex flex-col justify-center p-6 text-center lg:max-w-md xl:max-w-lg lg:text-left">
+              <button className="h-6 lg:-mt-40 mt-5 w-32 uppercase bg-white rounded-3xl text-purple-700">
+                Testimonial
+              </button>
+
+              <div className="lg:w-[750px] md:w-[450px] w-[320px] md:ml-32 lg:ml-0 mt-4 lg:mt-0">
+                <p className="lg:text-5xl md:text-4xl text-2xl my-4 text-[#fff]">
+                  Customer Review
+                </p>
+              </div>
+
+              <div className="flex gap-6">
+                {/* Total Review Section */}
+                <div>
+                  <p className="my-6">Total Review</p>
+                  <div className="flex gap-2 items-center">
+                    <h1 className="lg:text-5xl md:text-4xl text-2xl">122 K</h1>
+                    <button className="bg-blue-800 -mt-3 my-6 w-20 h-6 text-white font-semibold rounded-full bg-gradient-to-r from-blue-600 to-purple-600 flex items-center justify-center space-x-2">
+                      <span>24%</span>
+                      <BiUpArrowCircle className="text-lg md:text-xl" />
+                    </button>
+                  </div>
+                  <p className="my-6">Pulvinar commodo arcu</p>
+                </div>
+
+                {/* Average Rating Section */}
+                <div>
+                  <p className="my-6">Average Rating</p>
+                  <div className="flex gap-2 items-center">
+                    <h1 className="lg:text-5xl md:text-4xl text-2xl">4.8</h1>
+                    <div className="flex gap-1 -mt-3 my-6">
+                      <IoIosStar className="text-purple-400 text-xl" />
+                      <IoIosStar className="text-purple-400 text-xl" />
+                      <IoIosStar className="text-purple-400 text-xl" />
+                      <IoIosStar className="text-purple-400 text-xl" />
+                      <FaStarHalfAlt className="text-purple-400 text-xl" />
+                    </div>
+                  </div>
+                  <p className="my-6">Pulvinar commodo arcu</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Image Section */}
+            <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-[28rem] 2xl:h-[32rem]">
+              {/* <Image
+                src="https://via.placeholder.com/500"
+                alt="Customer review logo"
+                className="object-contain h-72 sm:h-80 lg:h-96 w-auto rounded-2xl"
+              /> */}
+              <img
+                src="https://via.placeholder.com/800x500"
+                alt="Customer review logo"
+                className="object-contain h-72 sm:h-80 lg:h-96 w-auto rounded-2xl"
+              />
+            </div>
+          </div>
+          <div className="bg-blue-600 border-b border-gray-300"></div>
+        </div>
       </div>
     </section>
   );
